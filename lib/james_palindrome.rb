7c6eae940e523f30e045f0bf62132584e7435f11
Jean-Philippe-94 <johnanag94@gmail.com>
2020-09-04 18:26:11 +0200

    Initialize repos

---
+++ lib/james_palindrome.rb
@@ -0,0 +1,6 @@
+require "james_palindrome/version"
+
+module JamesPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/james_palindrome/version.rb
@@ -0,0 +1,3 @@
+module JamesPalindrome
+  VERSION = "0.1.0"
+end
